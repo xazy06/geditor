3f6849ffd9bd48bc2699dbb3688f36efe0c8df1b
arthur33277 <xazy06@gmail.com>
2020-01-30 19:37:28 +0300

    refactor

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd ./app
+git checkout test
+git pull
+#sudo cp -R ./ ../
+
--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 var connect = require('connect');
+var http = require('http');
 var serveStatic = require('serve-static');
-connect().use(serveStatic('./')).listen(8080, function(){
-  console.log('Server running on 8080...');
+var bodyParser = require('body-parser');
+var port = 80;
+var app = connect();
+
+// gzip/deflate outgoing responses
+// var compression = require('compression');
+// app.use(compression());
+
+// store session state in browser cookie
+// var cookieSession = require('cookie-session');
+// app.use(cookieSession({
+//   keys: ['secret1', 'secret2']
+// }));
+
+app.use(serveStatic('./'))
+
+// parse urlencoded request bodies into req.body
+app.use(bodyParser.urlencoded({extended: false}));
+
+// respond to all requests
+app.use(function(req, res){
+  res.end('Hello from Connect!\n');
+});
+
+//create node.js http server and listen on port
+http.createServer(app).listen(port, function(){
+  console.log(`Server running on ${port}...`);
 });
