e0068608c250d3037e7b2fca58612d595a9c9a9b
arthur33277 <xazy06@gmail.com>
2020-02-06 18:58:23 +0300

    fix

--- js/extendEditorUi.js
+++ js/extendEditorUi.js
@@ -37,6 +37,252 @@
     // Main
     var main = new EditorUi(new Editor(urlParams['chrome'] == '0', themes));
     window.m = main;
+
+    var pallete = [];
+    api('GetPalette').then(function (data) {
+      pallete = data;
+    }).catch(function () {
+      pallete = [{
+        "displayName": "Ворота",
+        "className": "Gate",
+        "groupName": "Склад",
+        "frame": {
+          "x1": 0,
+          "y1": 0,
+          "x2": 300,
+          "y2": 3000
+        },
+        "graphics": [
+          {
+            "frame": {
+              "x1": 0,
+              "y1": 0,
+              "x2": 300,
+              "y2": 3000
+            },
+            "shapeName": "Rectangle"
+          }
+        ],
+        "defaultSize": {
+          "x1": 0,
+          "y1": 0,
+          "x2": 300,
+          "y2": 3000
+        },
+        "minSize": {
+          "x1": 0,
+          "y1": 0,
+          "x2": 100,
+          "y2": 2000
+        },
+        "maxSize": {
+          "x1": 0,
+          "y1": 0,
+          "x2": 1000,
+          "y2": 6000
+        },
+        "defaultText": "Ворота",
+        "properties": [
+          {
+            "displayName": "Номер ворот",
+            "propName": "Nr",
+            "groupName": "Общее",
+            "propType": "String",
+            "dataSource": {
+              "url": "",
+              "jsMethod": ""
+            },
+            "onChange": {
+              "url": "",
+              "jsMethod": ""
+            }
+          },
+          {
+            "displayName": "Штрихкод",
+            "propName": "Barcode",
+            "groupName": "Общее",
+            "propType": "String",
+            "dataSource": {
+              "url": "",
+              "jsMethod": ""
+            },
+            "onChange": {
+              "url": "",
+              "jsMethod": ""
+            }
+          }
+        ],
+        "onDblClick": {
+          "url": "",
+          "jsMethod": ""
+        },
+        "onChange": {
+          "url": "",
+          "jsMethod": ""
+        }
+      },
+        {
+          "displayName": "Стеллаж",
+          "className": "Rack",
+          "groupName": "Склад",
+          "frame": {
+            "x1": 0,
+            "y1": 0,
+            "x2": 300,
+            "y2": 3000
+          },
+          "graphics": [
+            {
+              "frame": {
+                "x1": 0,
+                "y1": 0,
+                "x2": 1000,
+                "y2": 2708
+              },
+              "shapeName": "Rectangle"
+            }
+          ],
+          "defaultSize": {
+            "x1": 0,
+            "y1": 0,
+            "x2": 1000,
+            "y2": 2708
+          },
+          "minSize": {
+            "x1": 0,
+            "y1": 0,
+            "x2": 1000,
+            "y2": 2708
+          },
+          "maxSize": {
+            "x1": 0,
+            "y1": 0,
+            "x2": 1000,
+            "y2": 2708
+          },
+          "defaultText": "Стеллаж",
+          "properties": [
+            {
+              "displayName": "Номер",
+              "propName": "Nr",
+              "groupName": "Общее",
+              "propType": "String",
+              "dataSource": {
+                "url": "",
+                "jsMethod": ""
+              },
+              "onChange": {
+                "url": "",
+                "jsMethod": ""
+              }
+            },
+            {
+              "displayName": "Штрихкод",
+              "propName": "Barcode",
+              "groupName": "Общее",
+              "propType": "String",
+              "dataSource": {
+                "url": "",
+                "jsMethod": ""
+              },
+              "onChange": {
+                "url": "",
+                "jsMethod": ""
+              }
+            }
+          ],
+          "onDblClick": {
+            "url": "",
+            "jsMethod": ""
+          },
+          "onChange": {
+            "url": "",
+            "jsMethod": ""
+          }
+        },
+        {
+          "displayName": "Стеллаж",
+          "className": "Rack",
+          "groupName": "",
+          "frame": {
+            "x1": 0,
+            "y1": 0,
+            "x2": 300,
+            "y2": 3000
+          },
+          "graphics": [
+            {
+              "frame": {
+                "x1": 0,
+                "y1": 0,
+                "x2": 1000,
+                "y2": 2708
+              },
+              "shapeName": "Rectangle"
+            }
+          ],
+          "defaultSize": {
+            "x1": 0,
+            "y1": 0,
+            "x2": 1000,
+            "y2": 2708
+          },
+          "minSize": {
+            "x1": 0,
+            "y1": 0,
+            "x2": 1000,
+            "y2": 2708
+          },
+          "maxSize": {
+            "x1": 0,
+            "y1": 0,
+            "x2": 1000,
+            "y2": 2708
+          },
+          "defaultText": "Стеллаж",
+          "properties": [
+            {
+              "displayName": "Номер",
+              "propName": "Nr",
+              "groupName": "Общее",
+              "propType": "String",
+              "dataSource": {
+                "url": "",
+                "jsMethod": ""
+              },
+              "onChange": {
+                "url": "",
+                "jsMethod": ""
+              }
+            },
+            {
+              "displayName": "Штрихкод",
+              "propName": "Barcode",
+              "groupName": "Общее",
+              "propType": "String",
+              "dataSource": {
+                "url": "",
+                "jsMethod": ""
+              },
+              "onChange": {
+                "url": "",
+                "jsMethod": ""
+              }
+            }
+          ],
+          "onDblClick": {
+            "url": "",
+            "jsMethod": ""
+          },
+          "onChange": {
+            "url": "",
+            "jsMethod": ""
+          }
+        }
+      ];
+
+      m.sidebar.addTopologyPallete(true);
+    });
   }, function () {
     document.body.innerHTML = '<div style="margin:10% auto 0;">Error loading resource files. Please check browser console.</div>';
   });
---
+++ upload.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ProxyCommand ssh root@134.0.113.190
+expect "assword:"
+send "raihien9Eeto\r"
+interact
+
+
+
