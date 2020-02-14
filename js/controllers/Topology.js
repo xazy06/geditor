b1c916084f3998559edb7047786d5ae6e05cc3fd
arthur33277 <xazy06@gmail.com>
2020-02-14 09:31:01 +0300

    converter topology model to mx model

--- js/controllers/Topology.js
+++ js/controllers/Topology.js
@@ -69,6 +69,33 @@ var Topology = function (mx) {
    * Назначение: Имя скрипта для сохранения чертежа;
    */
   this.SaveModelScript = null;
+
+  function MxModel() {
+    this.mxGraphModel = {
+      root: {
+        mxCell: [{
+          _id: '0'
+        }, {
+          _id: '1',
+          _parent: '0'
+        }]
+      },
+      _dx: '',
+      _dy: '',
+      _grid: '1',
+      _gridSize: '',
+      _guides: '1',
+      _tooltips: '1',
+      _connect: '1',
+      _arrows: '1',
+      _fold: '1',
+      _page: '1',
+      _pageScale: '1',
+      _pageWidth: '827',
+      _pageHeight: '1169'
+    };
+  }
+
   /**
    *
    * @type {{New: Topology.actions.New, Load: Topology.actions.Load, getGraphJSON: (function(): *), getGraphXml: (function(): (*|string)), Save: Topology.actions.Save}}
@@ -90,6 +117,222 @@ var Topology = function (mx) {
       return _this.x2js.xml_str2json(this.getGraphXml());
     },
 
+    parseTopologyModel (topologyModel) {
+      var mxModel = new MxModel();
+
+      topologyModel = topologyModel ||
+      {
+        "BgColor": "#4D4D4D",
+        "History": {
+          "Center": {
+            "X1": 928,
+              "Y1": 294
+          },
+          "Scale": 0.25400050800101603,
+            "Unit": "mm",
+            "ShowGrid": true,
+            "GridScale": 2.5,
+            "SnapToGrid": true,
+            "SnapToNearest": true,
+            "SnapStrength": 1
+        },
+        "Items": [
+          {
+            "Uuid": "2",
+            "ClassName": "Rack",
+            "Font": {
+              "Weight": 400,
+              "Bold": false,
+              "Italic": false,
+              "Underline": false
+            },
+            "Text": "Стеллаж",
+            "Layer": "1",
+            "Frame": {
+              "X1": 120,
+              "X2": 220,
+              "Y1": 80,
+              "Y2": 350.8
+            },
+            "Angle": 0,
+            "PropValues": [
+              {
+                "Номер": "ef"
+              },
+              {
+                "Штрихкод": "efe"
+              }
+            ]
+          }
+        ]
+      };
+
+      if(topologyModel.BgColor) {
+        mxModel.mxGraphModel._background = topologyModel.BgColor;
+      }
+
+      mxModel.mxGraphModel._dx = topologyModel.History.Center.X1;
+      mxModel.mxGraphModel._dy = topologyModel.History.Center.Y1;
+      mxModel.mxGraphModel._grid = +topologyModel.History.ShowGrid;
+      mxModel.mxGraphModel._gridSize = 10;
+      mxModel.mxGraphModel._pageScale=  "1";
+
+      function SimpleCellModel() {
+        this._value = null;
+        this.__id = null;
+        this.mxGeometry = null;
+        this._style = null;
+        this._className = null;
+        this._vertex = null;
+      }
+
+      function Layer(id, parent) {
+        this._id = id;
+        this._parent = '0';
+
+        if(parent !== undefined) {
+          this._parent = parent;
+        }
+      }
+
+      function MxGeometry(TFrame) {
+        this._x = TFrame.X1;
+        this._y = TFrame.Y1;
+        this._width = TFrame.X2 - TFrame.X1;
+        this._height = TFrame.Y2 - TFrame.Y1;
+      }
+
+      function parseTStyles(TModel) {
+        var stylePropsFromT = {
+          rotation: TModel.Angle,
+          strokeColor: TModel.FrontColor,
+          fillColor: TModel.BgColor,
+          fontColor: TModel.Font.Color,
+          fontFamily: TModel.Font.FontName,
+          fontSize: TModel.Font.Weight,
+          fontStyle: TModel.Font.Bold && '7' || '4',
+          whiteSpace: 'wrap',
+          html: '1',
+          rounded: '0'
+        };
+
+        _.each(stylePropsFromT, function (v, k) {
+          if(v === undefined) {
+            delete stylePropsFromT[k];
+          }
+        });
+
+        stylePropsFromT = _.map(stylePropsFromT, function(v, k) {
+          return k + '=' + v;
+        });
+
+        return (stylePropsFromT.join(';') + ';')
+      }
+
+      function addAttributes2Cell(attrs, cell) {
+        var obj = {
+          'object': {
+            _label: cell._value,
+            _id: cell._id
+
+          }
+        };
+
+
+        delete cell._id;
+        delete cell._value;
+
+        obj['object'].mxCell = cell;
+
+        _.each(attrs, function(val) {
+          var key = _.keys(val)[0];
+          obj['object'][('_' + key)] = val[key];
+        });
+
+        return obj;
+      }
+
+      function checkExecutedLayers(sourceItems, target) {
+        var layersUsedInT = [],
+          defaultLayersInMx = [],
+          layersLess = false,
+          idListOfAddedLayers = [];
+
+        _.each(sourceItems, function(item) {
+          if(layersUsedInT.length === 0) {
+            layersUsedInT.push(item.Layer);
+          }else if(layersUsedInT.indexOf(item.Layer) === -1) {
+            layersUsedInT.push(item.Layer);
+          }
+        });
+
+        defaultLayersInMx = _.map(target, function (c) {
+          return c._id;
+        });
+
+        for (var i in layersUsedInT) {
+
+          if(layersUsedInT.hasOwnProperty(i)) {
+
+            layersLess = false;
+
+            if(defaultLayersInMx.indexOf(layersUsedInT[i]) === -1) {
+              target.push(new Layer(layersUsedInT[i]));
+              idListOfAddedLayers.push(layersUsedInT[i]);
+
+              layersLess = true;
+            }
+          }
+        }
+
+        window.console && window.console.log({
+          layersLess: layersLess,
+          idListOfAddedLayers: idListOfAddedLayers
+        });
+
+        return {
+          layersLess: layersLess,
+          idListOfAddedLayers: idListOfAddedLayers
+        };
+      }
+
+      function appendCells(sourceItems, target) {
+        _.map(sourceItems, function(item) {
+          var cell = new SimpleCellModel();
+
+          cell._id = item.Uuid;
+          cell.__id = item.Text;
+          cell._value = item.Text;
+          cell._className = item.ClassName;
+          cell._vertex = "1";
+          cell._parent = item.Layer;
+          cell.mxGeometry = new MxGeometry(item.Frame);
+          cell._style = parseTStyles(item);
+
+          if(item.PropValues) {
+            cell = addAttributes2Cell(item.PropValues, cell);
+          }
+
+          target.push(cell);
+        })
+
+      }
+
+      checkExecutedLayers(topologyModel.Items, mxModel.mxGraphModel.root.mxCell);
+
+      appendCells(topologyModel.Items, mxModel.mxGraphModel.root.mxCell);
+
+      return mxModel;
+    },
+
+    parseTopologyModel2XML: function(tm) {
+      return _this.x2js.json2xml(this.parseTopologyModel(tm));
+    },
+
+    /**
+     *
+     * @returns {Model}
+     */
     parseGraph: function () {
       var model = null, gModel = null;
 
@@ -120,9 +363,9 @@ var Topology = function (mx) {
 
         history.Center = new Point(
           //_this.editor.editor.graph.getGraphBounds().getCenterX(),
-          _this.editor.graph.view.translate.x,
+          _this.editor.editor.graph.view.translate.x,
           //_this.editor.editor.graph.getGraphBounds().getCenterY()
-          _this.editor.graph.view.translate.y
+          _this.editor.editor.graph.view.translate.y
         );
 
         history.Scale = 1 / mxConstants.PIXELS_PER_MM;
